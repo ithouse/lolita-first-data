5766c59c40770f48609193928cae6589c0efdda3
Aivils Štoss <aivils@ithouse.lv>
2011-11-10 12:34:31 +0200

    fixed ::I18n against lolita-i18n

--- lib/lolita-first-data/custom_logger.rb
+++ lib/lolita-first-data/custom_logger.rb
@@ -3,7 +3,7 @@ module Lolita
     # custom log formatter for FirstData gateway
     class LogFormatter < Logger::Formatter
       def call(severity, time, program_name, message)
-        "%5s [%s] (%s) %s :: %s\n" % [severity,I18n.l(time), $$, program_name, message]
+        "%5s [%s] (%s) %s :: %s\n" % [severity,::I18n.l(time), $$, program_name, message]
       end
     end
   end
