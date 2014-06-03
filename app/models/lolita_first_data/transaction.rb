f148588b06a50bfe274b92816700b20c2e899247
Gatis Tomsons <gatis@ithouse.cc>
2014-06-03 12:16:19 +0300

    fixed transaction ip decode

--- app/models/lolita_first_data/transaction.rb
+++ app/models/lolita_first_data/transaction.rb
@@ -7,7 +7,7 @@ module LolitaFirstData
     validates_associated :paymentable
 
     def ip
-      IPAddr.new(self[:ip], Socket::AF_INET).to_s
+      IPAddr.new(self[:ip].to_i, Socket::AF_INET).to_s
     end
 
     def ip=(x)
